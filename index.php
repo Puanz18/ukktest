ab7dbaccc1995a193a442fb0e5dcdfa48482f5c1
Puanz18 <psyafwan@gmail.com>
2023-02-02 15:35:04 +0700

    ukktest

--- index.php
+++ index.php
@@ -34,10 +34,6 @@
                         <label for="exampleInputPassword1" class="form-label">Password</label>
                         <input type="password" class="form-control" id="exampleInputPassword1">
                     </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
                     <div class="d-gird gap-2">
                     <button type="submit" class="btn btn-primary">Gas kan</button>
                     </div>
