1c405c3050cc944b61a13687f068bfb6f6ca4a4d
Puanz18 <psyafwan@gmail.com>
2023-02-02 14:48:56 +0700

    ukktest

---
+++ dashboard/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="icon" href="../img/121.jpg">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<audio hidden autoplay loop>
+		<source src="../img/amvSong.mp3" type="audio/mpeg">
+		Browsermu tidak mendukung tag audio
+	  </audio>
+    <div class="hero">
+        <video autoplay loop muted plays-inline class="back-video">
+            <source src="../img/11.mp4" type="video/mp4">
+        </video>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="icon" href="img/121.jpg">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+<audio hidden autoplay loop>
+		<source src="img/Devil.mp3" type="audio/mpeg">
+		Browsermu tidak mendukung tag audio
+	  </audio>
+    <div class="hero">
+        <video autoplay loop muted plays-inline class="back-video">
+            <source src="img/12.mp4" type="video/mp4">
+        </video>
+    </div>
+    <div class="global-container">
+        <div class="card login-form">
+            <div class="card-body">
+                <h1 class="card-title text-center">L O G I N</h1>
+            </div>
+            <div class="card-text">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <div class="d-gird gap-2">
+                    <a href="www.youtube.com">
+                    <button type="submit" class="btn btn-primary">Gas kan</button></a>
+                    </div>
+                    <div class="d-gird gap-2">
+                    <button type="submit" class="btn btn-primary" class="button">Daftar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
