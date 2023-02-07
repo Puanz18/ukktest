a1d54809216ef1f0869c6dc09126b1e900e53466
Puanz18 <psyafwan@gmail.com>
2023-02-07 17:52:55 +0700

    ukktest

--- dashboard/index.php
+++ dashboard/index.php
@@ -109,7 +109,7 @@ if (empty($_SESSION['username'])) {
   <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-      <span class="fs-4">Sidebar</span>
+      <span class="fs-4">SPP</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
@@ -147,14 +147,10 @@ if (empty($_SESSION['username'])) {
     <hr>
     <div class="dropdown">
       <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
+        <img src="../img/121.jpg" alt="" width="32" height="32" class="rounded-circle me-2">
+        <strong>Puanz</strong>
       </a>
       <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
         <li><a class="dropdown-item" href="../proses/proses_logout.php">Sign out</a></li>
       </ul>
     </div>
