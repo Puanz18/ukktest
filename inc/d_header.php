f2e4eb3d5bdffdc7e5f5ae20a7abddc08b81fbac
Puanz18 <psyafwan@gmail.com>
2023-02-08 11:01:22 +0700

    d_header.php

--- inc/d_header.php
+++ inc/d_header.php
@@ -3,9 +3,12 @@
     <nav aria-label="menu nav" class="bg-gray-800 pt-2 md:pt-1 pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0">
         <div class="flex flex-wrap item-center">
             <div class="flex flex-shirivk md:w1/3 justify-center md:justify-start test-white">
-                <a href="#" aria-label="Home"></a>
+                <a href="#" aria-label="Home">
+                    <span class="test-x1 p1-2"><i class="em em-grinning"></i></span>
+                </a>
 
             </div>
         </div>
+        <div clas
     </nav>
 </header>
