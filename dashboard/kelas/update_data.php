b31ce0781a846d71813942b48ab6685bb280816d
Puanz18 <psyafwan@gmail.com>
2023-02-08 16:22:37 +0700

    ukktest

---
+++ dashboard/kelas/update_data.php
@@ -0,0 +1,9 @@
+<form action="../proses/proses_addkelas.php" method="POST">
+    <label for="nama_kelas">Nama kelas</label>
+    <input type="text" name="nama_kelas">
+    <br>
+    <label for="kompetensi_keahlian">Jurusan</label>
+<input type="text" name="kompetensi_keahlian">
+<br>
+<input type="submit" value="Tambah Data" name="submit">
+</form>
