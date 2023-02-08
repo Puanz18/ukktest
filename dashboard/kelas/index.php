800ae8446e7d0ba93faf97e97a10c389f700989e
Puanz18 <psyafwan@gmail.com>
2023-02-08 16:26:10 +0700

    ukktest

---
+++ dashboard/kelas/index.php
@@ -0,0 +1,12 @@
+<table>
+    <thead>
+        <tr>
+            <td>No</td>
+            <td>ID Kelas</td>
+            <td>Jurusan</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?= $kelas->tampilKelas(); ?>
+    </tbody>
+</table>
