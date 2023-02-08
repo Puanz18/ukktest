edba01b611572f0797fd14b30a4614d2b604e5e4
Puanz18 <psyafwan@gmail.com>
2023-02-08 10:27:03 +0700

    ukktest

--- inc/d_header.php
+++ inc/d_header.php
@@ -3,7 +3,7 @@
     <nav aria-label="menu nav" class="bg-gray-800 pt-2 md:pt-1 pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0">
         <div class="flex flex-wrap item-center">
             <div class="flex flex-shirivk md:w1/3 justify-center md:justify-start test-white">
-                <a href="#" aria-label="Home"></a>
+                <a href="s" aria-label="Home"></a>
 
             </div>
         </div>
