dd122872bc7e5c44a6cda7efcbeeb37a6cac2101
Puanz18 <psyafwan@gmail.com>
2023-02-10 15:25:20 +0700

    ukktest

---
+++ dashboard/siswa/update_data.php
@@ -0,0 +1,30 @@
+<form action="../proses/proses_addkelas.php" method="post">
+    <label for="nama_kelas">nisn</label>
+    <input type="text" name="nisn">
+    <br>
+    <label for="kompetesi_keahlian">Nis</label>
+    <input type="text" name="nis">
+    <br>
+    <label for="kompetesi_keahlian">Nama</label>
+    <input type="text" name="nama">
+    <br>
+    <label for="kompetesi_keahlian">username</label>
+    <input type="text" name="username">
+    <br>
+    <label for="kompetesi_keahlian">Password</label>
+    <input type="password" name="password">
+    <br>
+    <label for="kompetesi_keahlian">iD Kelas</label>
+    <input type="text" name="id_kelas">
+    <br>
+    <label for="kompetesi_keahlian">alamat</label>
+    <input type="text" name="alamat">
+    <br>
+    <label for="kompetesi_keahlian">Nomor Telefon</label>
+    <input type="text" name="no_telp">
+    <br>
+    <label for="kompetesi_keahlian">ID SPP</label>
+    <input type="text" name="id_spp">
+    <br>
+    <input type="submit" value="Tambah Data" name="submit"> 
+</form>
