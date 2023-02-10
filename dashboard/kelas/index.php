1ee9cc5336c375b1a579698503eececb6aa656d9
Puanz18 <psyafwan@gmail.com>
2023-02-10 14:08:26 +0700

    ukktest

--- dashboard/kelas/index.php
+++ dashboard/kelas/index.php
@@ -1,18 +1,12 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <a href="?url=addkelas">Tambah Data</a>
-  </head>
-  <body>
-    <table>
+    <thead>
+            <td>No</td>
+            <td>ID Kelas</td>
+            <td>Nama Kelas</td>
+            <td>Jurusan</td>
+            <td>Action</td>
+        </tr>
+    </thead>
     <tbody>
-    <?= $kelas->tampilKelas(); ?>
+        <?= $kelas->tampilKelas(); ?>
     </tbody>
-    </table>    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  </body>
-</html>
+</table>
